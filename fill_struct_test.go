c3703fa3d4675a6556d56398f8d3bf8ecef1a3cc
Francis Stephens <francisstephens@gmail.com>
2025-02-27 19:50:15 +1300

    Simplify tests
    Just using one struct to exercise all the different types.

--- fill_struct_test.go
+++ fill_struct_test.go
@@ -7,111 +7,35 @@ import (
 )
 
 func TestFill_int(t *testing.T) {
-	type intstruct struct {
-		Value int
+	type testStruct struct {
+		IntValue    int
+		UintValue   uint
+		FloatValue  float64
+		BoolValue   bool
+		StringValue string
+	}
+
+	expected := testStruct{
+		IntValue:    -1,
+		UintValue:   1,
+		FloatValue:  1.234,
+		BoolValue:   true,
+		StringValue: "string",
 	}
 
 	// Test value
-	val := intstruct{}
+	val := testStruct{}
 	Fill(&val)
-	assert.Equal(t, -1, val.Value)
+	assert.Equal(t, expected, val)
 
 	// Test pointer
-	valp := &intstruct{}
+	valp := &testStruct{}
 	Fill(valp)
-	assert.Equal(t, -1, valp.Value)
+	assert.Equal(t, expected, *valp)
 
 	// Test pointer to pointer
-	var valpp *intstruct
+	var valpp *testStruct
 	Fill(&valpp)
 	assert.NotNil(t, valpp)
-	assert.Equal(t, -1, valpp.Value)
-}
-
-func TestFill_uint(t *testing.T) {
-	type uintstruct struct {
-		Value uint
-	}
-
-	// Test value
-	val := uintstruct{}
-	Fill(&val)
-	assert.Equal(t, uint(1), val.Value)
-
-	// Test pointer
-	valp := &uintstruct{}
-	Fill(valp)
-	assert.Equal(t, uint(1), valp.Value)
-
-	// Test pointer to pointer
-	var valpp *uintstruct
-	Fill(&valpp)
-	assert.NotNil(t, valpp)
-	assert.Equal(t, uint(1), valpp.Value)
-}
-
-func TestFill_float64(t *testing.T) {
-	type float64struct struct {
-		Value float64
-	}
-
-	// Test value
-	val := float64struct{}
-	Fill(&val)
-	assert.Equal(t, float64(1.234), val.Value)
-
-	// Test pointer
-	valp := &float64struct{}
-	Fill(valp)
-	assert.Equal(t, float64(1.234), valp.Value)
-
-	// Test pointer ot pointer
-	var valpp *float64struct
-	Fill(&valpp)
-	assert.NotNil(t, &valpp)
-	assert.Equal(t, float64(1.234), valpp.Value)
-}
-
-func TestFill_bool(t *testing.T) {
-	type boolstruct struct {
-		Value bool
-	}
-
-	// Test value
-	val := boolstruct{}
-	Fill(&val)
-	assert.Equal(t, true, val.Value)
-
-	// Test pointer
-	valp := &boolstruct{}
-	Fill(valp)
-	assert.Equal(t, true, valp.Value)
-
-	// Test pointer ot pointer
-	var valpp *boolstruct
-	Fill(&valpp)
-	assert.NotNil(t, &valpp)
-	assert.Equal(t, true, valpp.Value)
-}
-
-func TestFill_string(t *testing.T) {
-	type stringstruct struct {
-		Value string
-	}
-
-	// Test value
-	val := stringstruct{}
-	Fill(&val)
-	assert.Equal(t, "string", val.Value)
-
-	// Test pointer
-	valp := &stringstruct{}
-	Fill(valp)
-	assert.Equal(t, "string", valp.Value)
-
-	// Test pointer ot pointer
-	var valpp *stringstruct
-	Fill(&valpp)
-	assert.NotNil(t, &valpp)
-	assert.Equal(t, "string", valpp.Value)
+	assert.Equal(t, expected, *valpp)
 }
