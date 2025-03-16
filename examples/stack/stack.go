edf60fc99688974553f2d0eaa5f9fe3637f2fd29
Francis Stephens <francisstephens@gmail.com>
2025-03-16 19:48:20 +1300

    Adding stack fuzz example
    Here we implement a stack implementation which passes a small set of
    reasonable looking tests. The stack implementation has a problem where
    the integer which tracks the top position of the stack is only 16 bits
    wide and will overflow under fairly normal usage conditions. However the
    conditions are difficult to hit in conventional unit tests.
    
    We add a fuzzing test which will reliably uncover the overflow failure
    condition. A user should be able to run
    
    go test -fuzz=Fuzz
    
    and see a failing case very quickly.
    
    Adding comments to fuzz_test.go for stack

---
+++ examples/stack/stack.go
@@ -0,0 +1,37 @@
+package stack
+
+import "fmt"
+
+type Stack struct {
+	top  int16 // This will cause bugs on overflow
+	data []any
+}
+
+func New() *Stack {
+	return &Stack{
+		data: []any{},
+	}
+}
+
+func (s *Stack) Push(val any) {
+	s.top++
+	if len(s.data) < int(s.top) {
+		s.data = append(s.data, val)
+	} else {
+		s.data[s.top-1] = val
+	}
+}
+
+func (s *Stack) Pop() any {
+	if s.top == 0 {
+		return nil
+	}
+
+	val := s.data[s.top-1]
+	s.top--
+	return val
+}
+
+func (s *Stack) String() string {
+	return fmt.Sprintf("top: %d, data-length: %d", s.top, len(s.data))
+}
